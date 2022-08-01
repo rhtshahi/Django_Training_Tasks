8ac64e645ba8949b384011a6f982e6d42dad1b2b
unknown <iamshahirohit@gmail.com>
2022-08-01 17:44:28 +0545

    Updated

--- OOP/distance.py
+++ OOP/distance.py
@@ -8,7 +8,14 @@ class Point:
     def coordinates(self):
         print(f'Coordinates: (x,y) = ({self.x},{self.y})')
 
+    def __str__(self):
+        print('Using __str__')
+        return f'Coordinates: (x,y) = ({self.x},{self.y})'
+
 p1=Point(2,4)
 p2=Point(3,7)
 print(Point.coordinates(p1))
 print(Point.coordinates(p2))
+
+print(p1)
+print(p2)
