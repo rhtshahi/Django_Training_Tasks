3ed0994860e64023cf4b42848a64c4c7402f60ac
unknown <iamshahirohit@gmail.com>
2022-08-02 12:29:34 +0545

    Updated

---
+++ ListExercise2/letterCombination.py
@@ -0,0 +1,2 @@
+#Write a Python program to create and display all combinations of letters, selecting each letter from a different key in a dictionary. 
+d1= {'1':['a','b'], '2':['c','d']}
--- OOP/distance.py
+++ OOP/distance.py
@@ -1,3 +1,6 @@
+from cmath import sqrt
+
+
 class Point:
     def __init__(self, x=0, y=0):
         self.x=x
@@ -12,6 +15,10 @@ class Point:
         print('Using __str__')
         return f'Coordinates: (x,y) = ({self.x},{self.y})'
 
+    def distance(self, obj_b):
+        res = sqrt((self.x-obj_b.x)^2+(self.y-obj_b.y)^2)
+        print(res)
+
 p1=Point(2,4)
 p2=Point(3,7)
 print(Point.coordinates(p1))
@@ -19,3 +26,8 @@ print(Point.coordinates(p2))
 
 print(p1)
 print(p2)
+
+print('')
+print('Distance: ')
+dis=Point.distance(p1, p2)
+print(dis)
