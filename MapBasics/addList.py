7ef0753c0672194ebe24544bc410ad549e81f34f
unknown <iamshahirohit@gmail.com>
2022-08-03 09:56:50 +0545

    @classmethod and map

---
+++ MapBasics/addList.py
@@ -0,0 +1,25 @@
+#----Add two lists using map and lambda----#
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+add_list=[]
+
+for i in range(0, len(numbers1)):
+    x=(numbers1[i]+numbers2[i])
+    add_list.append(x)
+print(add_list)
+
+#---Using map---#
+# print('')
+# print('Using map')
+
+# num1 = [1, 2, 3]
+# num2 = [4, 5, 6]
+# newList=[]
+
+# def addLists(l1,l2):
+#     for i in range(0, 3):
+#         elements=[l1[i]+l2[i]]
+#         newList.append(elements)
+#     return newList
+# y=list(map(addLists,(num1) , (num2)))
+# print(y)
---
+++ OOP/classMethod.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, subject, level):
+        self.name=name
+        self.subject=subject
+        self.level=level
+
+    def getDetails(self):
+        print(f'Name: {self.name}, Subject: {self.subject}Level: {self.level}')
+
+    @classmethod
+    def getDetails_using_classmethod(cls, name, subject, level):
+        print(f'Student Name: {name},Student Level: {level}, Student Subject: {subject}')
+
+#--Without using class method--#
+#s1 object is able to access the method defined inside class Student
+print('Without using class method:')
+s1=Student('Rohit', 'BSc', 5)
+s1.getDetails()
+
+print('')
+print('Using class method:')
+s2=Student.getDetails_using_classmethod('abc', 'BBA', 4)
+#s2.getDetails_using_classmethod()==> Object cannot access classmethod
