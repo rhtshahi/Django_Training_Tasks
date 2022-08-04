77add70d01b43dafcb7ac2d3efd65ecc2d5bc65b
unknown <iamshahirohit@gmail.com>
2022-08-04 18:46:16 +0545

    OOP updated: Inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,75 @@
+'''
+1. Single Inheritance
+2. Multiple
+3. Multilevel
+4. Hierarchial
+5. Hybrid
+
+syntax: class child_class(parent_class1, parent_class2)
+'''
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.is_emp = False
+
+    def get_description(self):
+        print('This is a person class description.', self.name, self.is_emp)
+    
+    #single inheritance
+
+class Employee(Person):
+    def employee_status(self):
+        self.is_emp=True
+
+person=Person('ABC')
+print("Person", person.name, person.is_emp)
+print('person parent class calling method', person.get_description())
+
+employee = Employee('Rohit')
+employee.employee_status()
+print('employee', employee.name, employee.is_emp)
+print('employee class calling parent method')
+employee.get_description()
+#print(Employee.mro())
+
+#--When a parent class is inherited by more than one child or subclasses -> Hierarchial Inheritance--#
+class Staff(Person):
+
+    # Overriding init example
+    def __init__(self, name):
+        super(Staff, self).__init__(name)
+        self.is_emp=True
+
+    def get_staff_status(self):
+        return self.is_emp
+
+staff_obj=Staff('Staff1')
+print('Staff', staff_obj.name, staff_obj.is_emp)
+print('get staff status', staff_obj.get_staff_status())
+staff_obj.get_description()
+
+print('')
+print('DOGS')
+class Dog:
+    def __init__(self, name, color):
+        self.name=name
+        self.color=color
+
+    def get_color(self):
+        print('Parent class ', self.color)
+        return self.color
+
+class Labrador(Dog):
+
+    def __init__(self, name, color, fur='double-coated'):
+        super(Labrador,self).__init__(name, color)
+        self.fur=fur
+
+dog = Dog('Tom', 'black')
+lab_obj = Labrador('Mickey', 'Golden')
+print(dog.name, dog.color)
+print(lab_obj.name, lab_obj.color, lab_obj.fur)
+print(lab_obj.get_color())
+print(dog.get_color())
+# print(dog.fur)           # >> Error coz parent class has no breed_name
---
+++ OOP/inheritance2_vehicles.py
@@ -0,0 +1,44 @@
+# Classwork: Do for Vehicle with parameters brand, model and num of wheels. 
+# Inherit it to Car and Bike, override the num of wheels attribute for car and bike respectively. 
+
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
+
+    def get_vehicle_info(self):
+        print('')
+        print('Vehicle Details:')
+        print(f'Brand: {self.brand}, Model: {self.model}')
+        return (self.brand, self.model)
+
+class Car(Vehicle):
+    def __init__(self, brand, model, num_of_wheels=4):
+        super(Car, self).__init__(brand, model)
+        self.num_of_wheels=num_of_wheels
+    
+    def get_car_info(self):
+        print('')
+        print('Car Details:')
+        print(f'Brand: {self.brand}, Model: {self.model}, Number of Wheels: {self.num_of_wheels}')
+        return (self.brand, self.model, self.num_of_wheels)
+
+class Bike(Vehicle):
+    def __init__(self, brand, model, wheels=2):
+        super(Bike, self).__init__(brand, model)
+        self.wheels=wheels
+
+    def get_bike_info(self):
+        print('')
+        print('Bike Details:')
+        print(f'Brand: {self.brand}, Model: {self.model}, Number of Wheels: {self.wheels}')
+        return (self.brand, self.model, self.wheels)
+
+vehicle1=Vehicle('Tata', 'Tata-1')
+vehicle1.get_vehicle_info()
+
+car1=Car('Audi', 'A1')
+car1.get_car_info()
+
+bike1=Bike('Honda', "H-1")
+bike1.get_bike_info()
